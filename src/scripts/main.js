5dd4b9e0c064c848833b43eb5b33e5cc848da699
linska <linskatanya@gmail.com>
2026-04-20 19:13:09 +0300

    feat: parse list with data attributes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+function toNumber(str) {
+  const num = Number(str.replaceAll(/[^0-9]/g, ''));
+
+  return Number.isNaN(num) ? 0 : num;
+}
+
+function sortList(list) {
+  const arr = [...list.children];
+
+  arr.sort((a, b) => {
+    const aValue = toNumber(a?.dataset?.salary);
+    const bValue = toNumber(b?.dataset?.salary);
+
+    return bValue - aValue;
+  });
+
+  list.append(...arr);
+}
+
+function getEmployees(list) {
+  const arr = [...list.children];
+
+  return arr.map((item) => ({
+    name: item?.dataset?.name ?? '',
+    position: item?.dataset?.position ?? '',
+    salary: toNumber(item?.dataset?.salary),
+    age: toNumber(item?.dataset?.age),
+  }));
+}
+
+const listElement = document.querySelector('ul');
+
+sortList(listElement);
+getEmployees(listElement);
